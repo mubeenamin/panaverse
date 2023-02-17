a2888682d0a487a5b5f9f72e2edd9aea4a42d8d7
Mubeen Ameen <mubeenamin6@gmail.com>
2023-02-17 20:57:31 +0500

    next js project

--- src/app/component/cards.tsx
+++ src/app/component/cards.tsx
@@ -22,12 +22,13 @@ export default function Cards() {
       </div>
       <div className="md:flex space-y-8 md:space-y-0 md:space-x-4 ">
          {cardItems.map((link)=>(
-        <div key={link.name} className="group border-2 p-8 md:w-1/2 md:py-20 shadow-lg cursor-pointer hover:shadow-red-600 transition ease-in delay-150 hover:-translate-y-1 hover:scale-100 duration-300 rounded-md">
-         <Link href={link.link}/>   
+        <div key={link.name} className="group border-2 p-8 md:w-1/2 md:py-12 shadow-lg cursor-pointer hover:shadow-red-600 transition ease-in delay-150 hover:-translate-y-1 hover:scale-100 duration-300 rounded-md">
+         <Link href={link.link}>   
           <span className="flex justify-center text-4xl transition duration-500 ease-in-out group-hover:opacity-0">{link.name}</span>
-          <span className="opacity-0 flex justify-center text-2xl transition duration-500 ease-in-out group-hover:opacity-100">
+          <span className=" flex justify-center text-2xl transition duration-500 ease-in-out opacity-0 group-hover:opacity-100">
             {link.text}
           </span>
+          </Link>
         </div>
         ))}
       </div>
