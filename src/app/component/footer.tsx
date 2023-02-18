fdd9b6c17150b68639e919d5f446f4dabd8500fd
Mubeen Ameen <mubeenamin6@gmail.com>
2023-02-18 14:02:11 +0500

    footer updated

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
         
       ];
   return (
-    <main className="py-20 bg-black mt-8 pl-4 pr-4 md:flex md:space-x-16 2xl:pl-36 md:p-16">
+    <main className="py-20 bg-black mt-8 pl-4 pr-4 md:flex md:space-x-12 2xl:pl-36 md:p-16">
       <div className="md:w-1/2">
         <div className="flex py-4 border-l-4 border-red-600 pl-4">
           <Image src={"/logo01.png"} height={50} width={50} alt="footer-logo" />
@@ -44,7 +44,7 @@ export default function Footer() {
       </div>
       <div className="group mt-4 pl-4 pr-4">
         <span className="text-xl text-white font-semibold group-hover:border-l-2 group-hover:border-red-600 pl-2">Support</span>
-        {footerMenu.map((link)=>(
+        {footerMenu1.map((link)=>(
         <div className="text-slate-400 mt-4 pl-2 hover:text-white cursor-pointer" key={link.name}>
             {link.name}
         </div>
