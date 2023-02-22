9cd4829b464141ea7ca3a3eb97ba079a623f6f0a
Mubeen Ameen <mubeenamin6@gmail.com>
2023-02-22 15:57:10 +0500

    updated

--- src/app/component/section.tsx
+++ src/app/component/section.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import Typed from "react-typed";
+
 export default function MainSection() {
   return (
     <main className="w-full ">
